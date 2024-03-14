6f0a944311f52e4fcc6946bbfd15582a4025532d
SeungHyeon <namsh1125@naver.com>
2024-03-14 22:46:20 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/kea/keapoint/onlog/OnlogApplication.java
@@ -0,0 +1,13 @@
+package kea.keapoint.onlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/keapoint/onlog/OnlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.keapoint.onlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
