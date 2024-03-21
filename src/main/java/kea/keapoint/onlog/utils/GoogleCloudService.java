959baa9be5c2c4f6f7d6485016201175717905b9
SeungHyeon <namsh1125@naver.com>
2024-03-21 23:08:20 +0900

    Feat: Google Translation 사용해서 번역 기능 구현

--- src/main/java/kea/keapoint/onlog/utils/GoogleCloudService.java
+++ src/main/java/kea/keapoint/onlog/utils/GoogleCloudService.java
@@ -4,7 +4,11 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import kea.keapoint.onlog.dto.FileUploadResponseDto;
+import kea.keapoint.onlog.dto.TranslationResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -18,11 +22,47 @@ import java.time.LocalDateTime;
 @Service
 public class GoogleCloudService {
 
+    @Value("${gcp.translation.credentials.location}")
+    private String translationCredentialsLocation;
+
     @Value("${gcp.storage.credentials.location}")
     private String cloudStorageCredentialsLocation;
 
     @Value("${gcp.storage.bucket-name}")
     private String cloudStorageBucketName;
+
+    public TranslationResponseDto translation(String text, String sourceLanguage, String targetLanguage) throws IOException {
+        log.debug("Google Cloud Service를 이용한 번역을 수행합니다.");
+
+        try {
+            // 번역 서비스 설정
+            Translate translate = TranslateOptions.newBuilder()
+                    .setCredentials(
+                            GoogleCredentials.fromStream(ResourceUtils.getURL(translationCredentialsLocation).openStream())
+                    )
+                    .build()
+                    .getService();
+
+            // 번역
+            Translation translation = translate.translate(
+                    text,
+                    Translate.TranslateOption.sourceLanguage(sourceLanguage),
+                    Translate.TranslateOption.targetLanguage(targetLanguage)
+            );
+
+            // 번역 결과 반환
+            return TranslationResponseDto.builder()
+                    .originalText(text)
+                    .translatedText(translation.getTranslatedText())
+                    .sourceLanguage(sourceLanguage)
+                    .targetLanguage(targetLanguage)
+                    .build();
+
+        } catch (Exception e) {
+            log.error("Google Cloud Service를 이용한 번역 중 오류가 발생했습니다.", e);
+            throw e;
+        }
+    }
 
     public FileUploadResponseDto uploadFile(String filePath, MultipartFile file) throws IOException {
         log.debug("Google Cloud Service에 파일을 업로드합니다.");
